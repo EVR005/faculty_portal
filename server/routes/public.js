803144d35be0ac839240f4c2c564c6d45d5e87a5
Edumba Vannia Raja <edumbasankaravel513@gmail.com>
2026-06-29 22:11:51 +0530

    Added Public health check api

--- server/routes/public.js
+++ server/routes/public.js
@@ -2,7 +2,6 @@ const express = require("express");
 const publicRouter = express.Router();
 const publicService = require("../controllers/PublicController");
 
-// router.post('/editDetails',faculty.)
 publicRouter.get("/health", publicService.checkHealth);
 
 module.exports = { publicRouter };
